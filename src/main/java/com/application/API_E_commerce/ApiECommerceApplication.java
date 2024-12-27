46df5a0f0aa307302d8f3c2698db35e9935a0ff8
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-12-27 15:43:05 -0300

    feat: initial commit

---
+++ src/main/java/com/application/API_E_commerce/ApiECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.application.API_E_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/API_E_commerce/ApiECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.API_E_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
