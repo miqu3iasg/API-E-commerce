a4b692ea2e78755f0ee2cba2579600b76d426fa0
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-12-30 22:13:46 -0300

    chore: add order and order item class

--- src/main/java/com/application/API_E_commerce/domain/order/Order.java
+++ src/main/java/com/application/API_E_commerce/domain/order/Order.java
@@ -1,2 +1,89 @@
-package com.application.API_E_commerce.domain.order.orderitem;public class Order {
+package com.application.API_E_commerce.domain.order;
+
+import com.application.API_E_commerce.domain.order.orderitem.OrderItem;
+import com.application.API_E_commerce.domain.payment.Payment;
+import com.application.API_E_commerce.domain.user.User;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public class Order {
+  private UUID id;
+  private User user;
+  private String status; // pending, payment...
+  private LocalDateTime orderDate;
+  private BigDecimal totalValue;
+  private List<OrderItem> items;
+  private Payment payment;
+
+  public Order() {
+  }
+
+  public Order(UUID id, User user, String status, LocalDateTime orderDate, BigDecimal totalValue, List<OrderItem> items, Payment payment) {
+    this.id = id;
+    this.user = user;
+    this.status = status;
+    this.orderDate = orderDate;
+    this.totalValue = totalValue;
+    this.items = items;
+    this.payment = payment;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public LocalDateTime getOrderDate() {
+    return orderDate;
+  }
+
+  public void setOrderDate(LocalDateTime orderDate) {
+    this.orderDate = orderDate;
+  }
+
+  public BigDecimal getTotalValue() {
+    return totalValue;
+  }
+
+  public void setTotalValue(BigDecimal totalValue) {
+    this.totalValue = totalValue;
+  }
+
+  public List<OrderItem> getItems() {
+    return items;
+  }
+
+  public void setItems(List<OrderItem> items) {
+    this.items = items;
+  }
+
+  public Payment getPayment() {
+    return payment;
+  }
+
+  public void setPayment(Payment payment) {
+    this.payment = payment;
+  }
 }
--- src/main/java/com/application/API_E_commerce/domain/order/orderitem/OrderItem.java
+++ src/main/java/com/application/API_E_commerce/domain/order/orderitem/OrderItem.java
@@ -1,2 +1,55 @@
-package com.application.API_E_commerce.domain.order.orderitem;public class OrderItem {
+package com.application.API_E_commerce.domain.order.orderitem;
+
+import com.application.API_E_commerce.domain.order.Order;
+import com.application.API_E_commerce.domain.product.Product;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class OrderItem {
+  private UUID id;
+  private Order order;
+  private Product product;
+  private int quantity;
+  private BigDecimal unitPrice;
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public Order getOrder() {
+    return order;
+  }
+
+  public void setOrder(Order order) {
+    this.order = order;
+  }
+
+  public Product getProduct() {
+    return product;
+  }
+
+  public void setProduct(Product product) {
+    this.product = product;
+  }
+
+  public int getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(int quantity) {
+    this.quantity = quantity;
+  }
+
+  public BigDecimal getUnitPrice() {
+    return unitPrice;
+  }
+
+  public void setUnitPrice(BigDecimal unitPrice) {
+    this.unitPrice = unitPrice;
+  }
 }
