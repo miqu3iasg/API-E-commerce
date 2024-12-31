31d4c0609a8599d808da4dae0cb43f7426280aca
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-12-30 22:12:37 -0300

    chore: add cart class

--- src/main/java/com/application/API_E_commerce/domain/cart/Cart.java
+++ src/main/java/com/application/API_E_commerce/domain/cart/Cart.java
@@ -1,2 +1,47 @@
-package com.application.API_E_commerce.domain.cart;public class Cart {
+package com.application.API_E_commerce.domain.cart;
+
+import com.application.API_E_commerce.domain.cart.cartitem.CartItem;
+import com.application.API_E_commerce.domain.user.User;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public class Cart {
+  private UUID id;
+  private User user;
+  private LocalDateTime createdAt;
+  private List<CartItem> items;
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(LocalDateTime createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public List<CartItem> getItems() {
+    return items;
+  }
+
+  public void setItems(List<CartItem> items) {
+    this.items = items;
+  }
 }
--- src/main/java/com/application/API_E_commerce/domain/cart/cartitem/CartItem.java
+++ src/main/java/com/application/API_E_commerce/domain/cart/cartitem/CartItem.java
@@ -1,2 +1,55 @@
-package com.application.API_E_commerce.domain.cart.cartitem;public class CartItem {
+package com.application.API_E_commerce.domain.cart.cartitem;
+
+import com.application.API_E_commerce.domain.cart.Cart;
+import com.application.API_E_commerce.domain.product.Product;
+
+import java.util.UUID;
+
+public class CartItem {
+  private UUID id;
+  private Cart cart;
+  private Product product;
+  private int quantity;
+
+  public CartItem() {
+  }
+
+  public CartItem(UUID id, Cart cart, Product product, int quantity) {
+    this.id = id;
+    this.cart = cart;
+    this.product = product;
+    this.quantity = quantity;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public Cart getCart() {
+    return cart;
+  }
+
+  public void setCart(Cart cart) {
+    this.cart = cart;
+  }
+
+  public Product getProduct() {
+    return product;
+  }
+
+  public void setProduct(Product product) {
+    this.product = product;
+  }
+
+  public int getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(int quantity) {
+    this.quantity = quantity;
+  }
 }
