47fd66440c4978df965d1c6fb6be6a5830e96703
Miquéias Alves <miqueiasalves.developer@gmail.com>
2024-12-30 22:14:32 -0300

    chore: add product class

--- src/main/java/com/application/API_E_commerce/domain/product/Product.java
+++ src/main/java/com/application/API_E_commerce/domain/product/Product.java
@@ -1,2 +1,96 @@
-package com.application.API_E_commerce;public class Product {
+package com.application.API_E_commerce.domain.product;
+
+import com.application.API_E_commerce.domain.category.Category;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Product {
+  private UUID id;
+  private String name;
+  private String description;
+  private BigDecimal price;
+  private int stock;
+  private Category category;
+  private String imgUrl;
+  private LocalDateTime createdAt;
+
+  public Product() {
+  }
+
+  public Product(UUID id, String name, String description, BigDecimal price, int stock, Category category, String imgUrl, LocalDateTime createdAt) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.stock = stock;
+    this.category = category;
+    this.imgUrl = imgUrl;
+    this.createdAt = createdAt;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public BigDecimal getPrice() {
+    return price;
+  }
+
+  public void setPrice(BigDecimal price) {
+    this.price = price;
+  }
+
+  public int getStock() {
+    return stock;
+  }
+
+  public void setStock(int stock) {
+    this.stock = stock;
+  }
+
+  public Category getCategory() {
+    return category;
+  }
+
+  public void setCategory(Category category) {
+    this.category = category;
+  }
+
+  public String getImgUrl() {
+    return imgUrl;
+  }
+
+  public void setImgUrl(String imgUrl) {
+    this.imgUrl = imgUrl;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(LocalDateTime createdAt) {
+    this.createdAt = createdAt;
+  }
 }
