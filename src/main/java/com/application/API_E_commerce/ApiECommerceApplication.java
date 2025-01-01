8f1f23fd778047a2b99280b3325d9e384245500e
Miquéias Alves <miqueiasalves.developer@gmail.com>
2025-01-01 17:20:22 -0300

    chore: add scan annotations

--- src/main/java/com/application/API_E_commerce/ApiECommerceApplication.java
+++ src/main/java/com/application/API_E_commerce/ApiECommerceApplication.java
@@ -2,8 +2,10 @@ package com.application.API_E_commerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.application.API_E_commerce")
+@EntityScan("com.application.API_E_commerce.adapters.outbound.entities")
 public class ApiECommerceApplication {
 
 	public static void main(String[] args) {
