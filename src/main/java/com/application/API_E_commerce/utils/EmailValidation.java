14f3bf6688fc68451a4199be578e216f6442ec68
Miquéias Alves <miqueiasalves.developer@gmail.com>
2025-01-01 17:25:36 -0300

    feat: add email validation annotation

--- src/main/java/com/application/API_E_commerce/utils/EmailValidation.java
+++ src/main/java/com/application/API_E_commerce/utils/EmailValidation.java
@@ -1,2 +1,18 @@
-package com.application.API_E_commerce.utils;public class EmailValidation {
+package com.application.API_E_commerce.utils;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = EmailValidator.class)
+@Target({ ElementType.FIELD, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EmailValidation {
+  String message() default "Invalid email format.";
+  Class<?>[] groups() default {};
+  Class<? extends Payload>[] payload() default {};
 }
