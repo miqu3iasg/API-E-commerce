9a709d00a5dbee0c2e02616826d69b74565eb40d
Miquéias Alves <miqueiasalves.developer@gmail.com>
2025-01-02 15:49:01 -0300

    fix: solve contextloads teste error

--- src/test/java/com/application/API_E_commerce/ApiECommerceApplicationTests.java
+++ src/test/java/com/application/API_E_commerce/ApiECommerceApplicationTests.java
@@ -1,13 +1,8 @@
 package com.application.API_E_commerce;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApiECommerceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
