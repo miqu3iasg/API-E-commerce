bb1f7758cc5f55ccf2527a6f5bb85880a76d10e3
Miquéias Alves <miqueiasalves.developer@gmail.com>
2025-01-06 22:14:46 -0300

    feat: add order item mapper

--- src/main/java/com/application/API_E_commerce/utils/mappers/OrderItemMapper.java
+++ src/main/java/com/application/API_E_commerce/utils/mappers/OrderItemMapper.java
@@ -1,2 +1,49 @@
-package com.application.API_E_commerce.utils.mappers;public interface OrderItemMapper {
+package com.application.API_E_commerce.utils.mappers;
+
+import com.application.API_E_commerce.adapters.outbound.entities.order.JpaOrderItemEntity;
+import com.application.API_E_commerce.adapters.outbound.entities.product.JpaProductEntity;
+import com.application.API_E_commerce.domain.order.orderitem.OrderItem;
+import com.application.API_E_commerce.domain.product.Product;
+import org.mapstruct.*;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = { ProductMapper.class })
+public interface OrderItemMapper {
+
+  OrderItemMapper INSTANCE = Mappers.getMapper(OrderItemMapper.class);
+
+  @Mappings({
+          @Mapping(source = "id", target = "id"),
+          @Mapping(source = "order", target = "order", ignore = true), // Evitar loop bidirecional
+          @Mapping(source = "product", target = "product", qualifiedByName = "productToJpa"),
+          @Mapping(source = "quantity", target = "quantity"),
+          @Mapping(source = "unitPrice", target = "unitPrice")
+  })
+  JpaOrderItemEntity toJpa(OrderItem domain);
+
+
+  @Mappings({
+          @Mapping(source = "id", target = "id"),
+          @Mapping(source = "order", target = "order", ignore = true),
+          @Mapping(source = "product", target = "product", qualifiedByName = "productToDomain"),
+          @Mapping(source = "quantity", target = "quantity"),
+          @Mapping(source = "unitPrice", target = "unitPrice")
+  })
+  OrderItem toDomain(JpaOrderItemEntity jpa);
+
+  @Named("orderItemsToJpa")
+  default List<JpaOrderItemEntity> orderItemsToJpa(List<OrderItem> items) {
+    return items == null
+            ? null
+            : items.stream().map(OrderItemMapper.INSTANCE::toJpa).toList();
+  }
+
+  @Named("orderItemsToDomain")
+  default List<OrderItem> orderItemsToDomain(List<JpaOrderItemEntity> items) {
+    return items == null
+            ? null
+            : items.stream().map(OrderItemMapper.INSTANCE::toDomain).toList();
+  }
 }
