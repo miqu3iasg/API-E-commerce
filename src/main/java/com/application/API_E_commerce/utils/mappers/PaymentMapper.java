f0e19c8ddebcc076d13bec03184f29ca1d47a7ba
Miquéias Alves <miqueiasalves.developer@gmail.com>
2025-01-06 22:15:49 -0300

    feat: add payment mapper

--- src/main/java/com/application/API_E_commerce/utils/mappers/PaymentMapper.java
+++ src/main/java/com/application/API_E_commerce/utils/mappers/PaymentMapper.java
@@ -1,2 +1,40 @@
-package com.application.API_E_commerce.utils.mappers;public interface PaymentMapper {
+package com.application.API_E_commerce.utils.mappers;
+
+import com.application.API_E_commerce.adapters.outbound.entities.payment.JpaPaymentEntity;
+import com.application.API_E_commerce.domain.payment.Payment;
+import org.mapstruct.*;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface PaymentMapper {
+
+  PaymentMapper INSTANCE = Mappers.getMapper(PaymentMapper.class);
+
+  @Mappings({
+          @Mapping(source = "id", target = "id"),
+          @Mapping(source = "order", target = "order", ignore = true),
+          @Mapping(source = "paymentMethod", target = "paymentMethod"),
+          @Mapping(source = "status", target = "status"),
+          @Mapping(source = "paymentDate", target = "paymentDate"),
+  })
+  JpaPaymentEntity toJpa(Payment domain);
+
+  @Mappings({
+          @Mapping(source = "id", target = "id"),
+          @Mapping(source = "order", target = "order", ignore = true),
+          @Mapping(source = "paymentMethod", target = "paymentMethod"),
+          @Mapping(source = "status", target = "status"),
+          @Mapping(source = "paymentDate", target = "paymentDate"),
+  })
+  Payment toDomain(JpaPaymentEntity jpa);
+
+  @Named("paymentToJpa")
+  default JpaPaymentEntity paymentToJpa(Payment payment) {
+    return payment == null ? null : PaymentMapper.INSTANCE.toJpa(payment);
+  }
+
+  @Named("paymentToDomain")
+  default Payment paymentToDomain(JpaPaymentEntity jpaPayment) {
+    return jpaPayment == null ? null : PaymentMapper.INSTANCE.toDomain(jpaPayment);
+  }
 }
