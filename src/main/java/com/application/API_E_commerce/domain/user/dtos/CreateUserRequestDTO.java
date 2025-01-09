e1f0be1d2166b3a6651bdd8379c22bcfa16b2861
Miquéias Alves <miqueiasalves.developer@gmail.com>
2025-01-08 22:13:50 -0300

    chore: add create user dto

--- src/main/java/com/application/API_E_commerce/domain/user/dtos/CreateUserRequestDTO.java
+++ src/main/java/com/application/API_E_commerce/domain/user/dtos/CreateUserRequestDTO.java
@@ -1,2 +1,13 @@
-package com.application.API_E_commerce.domain.user.dtos;public record CreateUserRequestDTO() {
+package com.application.API_E_commerce.domain.user.dtos;
+
+import com.application.API_E_commerce.domain.address.Address;
+import com.application.API_E_commerce.domain.user.UserRole;
+
+public record CreateUserRequestDTO (
+        String name,
+        String email,
+        String password,
+        UserRole role,
+        Address address
+) {
 }
