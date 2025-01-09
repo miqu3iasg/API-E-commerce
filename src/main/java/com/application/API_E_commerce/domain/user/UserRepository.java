05414e4991c56667d062daaf1da65a2079f27207
Miquéias Alves <miqueiasalves.developer@gmail.com>
2025-01-08 22:12:59 -0300

    feat: add find user by name and email in user repository

--- src/main/java/com/application/API_E_commerce/domain/user/UserRepository.java
+++ src/main/java/com/application/API_E_commerce/domain/user/UserRepository.java
@@ -7,7 +7,9 @@ import java.util.UUID;
 public interface UserRepository {
   User saveUser(User user);
   Optional<User> findUserById(UUID userId);
+  Optional<User> findUserByName(String username);
   List<User> findAllUsers();
   void deleteUser(User user);
   void deleteUserById(UUID userId);
+  Optional<User> findUserByEmail(String email);
 }
