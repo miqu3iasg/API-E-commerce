4a45b5d655f6c740c4d6ceace765bda8fa236703
Miquéias Alves <miqueiasalves.developer@gmail.com>
2025-01-08 22:11:01 -0300

    feat: add user use cases interface

---
+++ src/main/java/com/application/API_E_commerce/application/usecases/UserUseCases.java
@@ -0,0 +1,20 @@
+package com.application.API_E_commerce.application.usecases;
+
+import com.application.API_E_commerce.domain.address.Address;
+import com.application.API_E_commerce.domain.address.dtos.UpdateAddressRequestDTO;
+import com.application.API_E_commerce.domain.user.User;
+import com.application.API_E_commerce.domain.user.dtos.CreateUserRequestDTO;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserUseCases {
+  void createUser(CreateUserRequestDTO createUserRequest);
+  Optional<User> findUserById(UUID userId);
+  void updateUserName(UUID userId, String updatedNameRequest);
+  void updatedUserPassword(UUID userId, String updatedPasswordRequest);
+  Address updateUserAddress(UUID userId, UpdateAddressRequestDTO updatedAddressRequest);
+  void deleteUser(UUID userId);
+  List<User> findAllUsers();
+}
---
+++ src/main/java/com/application/API_E_commerce/domain/address/dtos/UpdateAddressRequestDTO.java
@@ -0,0 +1,2 @@
+package com.application.API_E_commerce.domain.address.dtos;public record UpdateAddressRequestDTO() {
+}
