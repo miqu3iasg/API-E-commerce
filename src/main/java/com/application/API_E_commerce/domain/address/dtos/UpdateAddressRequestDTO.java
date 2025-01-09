f640215474bfc4cf38400ab5b58dfc3339e0420a
Miquéias Alves <miqueiasalves.developer@gmail.com>
2025-01-08 22:15:41 -0300

    chore: add update user dto

--- src/main/java/com/application/API_E_commerce/domain/address/dtos/UpdateAddressRequestDTO.java
+++ src/main/java/com/application/API_E_commerce/domain/address/dtos/UpdateAddressRequestDTO.java
@@ -1,2 +1,4 @@
-package com.application.API_E_commerce.domain.address.dtos;public record UpdateAddressRequestDTO() {
+package com.application.API_E_commerce.domain.address.dtos;
+
+public record UpdateAddressRequestDTO (String street, String city, String state, String zipCode, String country) {
 }
