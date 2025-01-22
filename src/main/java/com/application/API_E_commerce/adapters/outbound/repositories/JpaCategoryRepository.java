c1d60506cbc2f9f48137a534cca9a76cbb45d0cc
Miquéias Alves <miqueiasalves.developer@gmail.com>
2025-01-22 16:29:10 -0300

    feat: add jpa category repository

--- src/main/java/com/application/API_E_commerce/adapters/outbound/repositories/JpaCategoryRepository.java
+++ src/main/java/com/application/API_E_commerce/adapters/outbound/repositories/JpaCategoryRepository.java
@@ -1,4 +1,11 @@
 package com.application.API_E_commerce.adapters.outbound.repositories;
 
-public interface JpaCategoryRepository {
+import com.application.API_E_commerce.adapters.outbound.entities.category.JpaCategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface JpaCategoryRepository extends JpaRepository<JpaCategoryEntity, UUID> {
 }
