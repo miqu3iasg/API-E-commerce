deed076ed8898fd4d7eb21d5c0470adaa70a26d1
Miquéias Alves <miqueiasalves.developer@gmail.com>
2025-01-22 16:35:23 -0300

    feat: add implementation of method find category by id in category service implementation

--- src/main/java/com/application/API_E_commerce/application/services/CategoryServiceImplementation.java
+++ src/main/java/com/application/API_E_commerce/application/services/CategoryServiceImplementation.java
@@ -1,4 +1,24 @@
 package com.application.API_E_commerce.application.services;
 
-public class CategoryServiceImplementation {
+import com.application.API_E_commerce.application.usecases.CategoryUseCases;
+import com.application.API_E_commerce.domain.category.Category;
+import com.application.API_E_commerce.domain.category.CategoryRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+public class CategoryServiceImplementation implements CategoryUseCases {
+  private final CategoryRepository categoryRepository;
+
+  public CategoryServiceImplementation(CategoryRepository categoryRepository) {
+    this.categoryRepository = categoryRepository;
+  }
+
+  @Override
+  public Optional<Category> findCategoryById(UUID categoryId) {
+    return Optional.ofNullable(this.categoryRepository.findCategoryById(categoryId)
+            .orElseThrow(() -> new IllegalArgumentException("Category cannot be null.")));
+  }
 }
