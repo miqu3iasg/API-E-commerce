e568fb1719f0086851085ea65b2a4fed134953b1
Miquéias Alves <miqueiasalves.developer@gmail.com>
2025-01-23 07:44:59 -0300

    feat: add category validator

--- src/main/java/com/application/API_E_commerce/utils/validators/CategoryValidator.java
+++ src/main/java/com/application/API_E_commerce/utils/validators/CategoryValidator.java
@@ -1,4 +1,27 @@
 package com.application.API_E_commerce.utils.validators;
 
+import com.application.API_E_commerce.domain.category.Category;
+import com.application.API_E_commerce.domain.category.CategoryRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
 public class CategoryValidator {
+  private final CategoryRepository categoryRepository;
+
+  public CategoryValidator(CategoryRepository categoryRepository) {
+    this.categoryRepository = categoryRepository;
+  }
+
+  public Category validateIfCategoryExistsAndReturnTheExistingCategory(UUID categoryId) {
+    Optional<Category> categoryOptional = this.categoryRepository.findCategoryById(categoryId);
+
+    if (categoryOptional.isEmpty()) {
+      throw new IllegalArgumentException("Category was not found.");
+    }
+
+    return categoryOptional.get();
+  }
 }
