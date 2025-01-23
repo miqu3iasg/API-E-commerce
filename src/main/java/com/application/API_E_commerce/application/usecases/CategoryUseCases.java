0d59433310157e8cd0031d7414ad2ed84057bdd8
Miquéias Alves <miqueiasalves.developer@gmail.com>
2025-01-23 07:32:11 -0300

    feat: add category use cases

--- src/main/java/com/application/API_E_commerce/application/usecases/CategoryUseCases.java
+++ src/main/java/com/application/API_E_commerce/application/usecases/CategoryUseCases.java
@@ -1,4 +1,10 @@
 package com.application.API_E_commerce.application.usecases;
 
+import com.application.API_E_commerce.domain.category.Category;
+
+import java.util.Optional;
+import java.util.UUID;
+
 public interface CategoryUseCases {
+  Optional<Category> findCategoryById(UUID categoryId);
 }
