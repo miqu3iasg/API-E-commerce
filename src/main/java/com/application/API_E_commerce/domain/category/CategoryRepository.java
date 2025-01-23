d82ceaf3469f6e921faa6cb02948960e1b9f6c50
Miquéias Alves <miqueiasalves.developer@gmail.com>
2025-01-23 07:35:13 -0300

    feat: add category repository

--- src/main/java/com/application/API_E_commerce/domain/category/CategoryRepository.java
+++ src/main/java/com/application/API_E_commerce/domain/category/CategoryRepository.java
@@ -1,4 +1,12 @@
 package com.application.API_E_commerce.domain.category;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface CategoryRepository {
+  Category saveCategory(Category category);
+  Optional<Category> findCategoryById(UUID categoryId);
+  List<Category> findAllCategories();
+  void deleteCategoryById(UUID categoryId);
 }
