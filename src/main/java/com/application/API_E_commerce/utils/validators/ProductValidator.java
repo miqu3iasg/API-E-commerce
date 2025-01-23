e5bae0c91c78f734459c5a20efebd88a42539563
Miquéias Alves <miqueiasalves.developer@gmail.com>
2025-01-23 07:45:31 -0300

    feat: add product validator

--- src/main/java/com/application/API_E_commerce/utils/validators/ProductValidator.java
+++ src/main/java/com/application/API_E_commerce/utils/validators/ProductValidator.java
@@ -1,23 +1,45 @@
-package com.application.API_E_commerce.application.services;
+package com.application.API_E_commerce.utils.validators;
 
+import com.application.API_E_commerce.adapters.outbound.entities.product.JpaProductEntity;
+import com.application.API_E_commerce.adapters.outbound.repositories.JpaProductRepository;
+import com.application.API_E_commerce.adapters.outbound.repositories.ProductRepositoryImplementation;
+import com.application.API_E_commerce.application.usecases.CategoryUseCases;
 import com.application.API_E_commerce.domain.category.Category;
+import com.application.API_E_commerce.domain.product.Product;
 import com.application.API_E_commerce.domain.product.dtos.CreateProductRequestDTO;
-import org.springframework.stereotype.Service;
+import com.application.API_E_commerce.domain.product.repository.ProductRepository;
+import com.application.API_E_commerce.utils.converters.ProductConverter;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.util.Optional;
+import java.util.UUID;
 
-@Service
+@Component
 public class ProductValidator {
-  public void validate(CreateProductRequestDTO request, Optional<Category> category) {
-    if (category.isEmpty()) {
-      throw new IllegalArgumentException("Category cannot be null.");
-    }
+  private final ProductRepository productRepository;
+
+  public ProductValidator(ProductRepository productRepository) {
+    this.productRepository = productRepository;
+  }
+
+  public void validateCreateProductRequest(CreateProductRequestDTO request) {
     if (request.price() == null || request.price().compareTo(BigDecimal.ZERO) <= 0) {
       throw new IllegalArgumentException("The price should be greater than zero.");
     }
+
     if (request.stock() < 0) {
       throw new IllegalArgumentException("Stock cannot be smaller than zero.");
     }
+  }
+
+  public Product validateIfProductExistsAndReturnTheExistingProduct(UUID productId) {
+    Optional<Product> productOptional = this.productRepository.findProductById(productId);
+
+    if (productOptional.isEmpty()) {
+      throw new IllegalArgumentException("Product was not found.");
+    }
+
+    return productOptional.get();
   }
 }
