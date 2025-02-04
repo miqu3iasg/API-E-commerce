2d7467a736b604acb2384019204539e37549df89
Miquéias Alves <miqueiasalves.developer@gmail.com>
2025-02-03 21:55:04 -0300

    chore: add payment use cases

--- src/main/java/com/application/API_E_commerce/application/usecases/PaymentUseCases.java
+++ src/main/java/com/application/API_E_commerce/application/usecases/PaymentUseCases.java
@@ -1,4 +1,7 @@
 package com.application.API_E_commerce.application.usecases;
 
+import com.application.API_E_commerce.domain.payment.Payment;
+
 public interface PaymentUseCases {
+  Payment processPayment(Payment payment);
 }
