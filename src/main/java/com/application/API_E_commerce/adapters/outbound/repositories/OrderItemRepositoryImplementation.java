ef2a6c94df49a6384eea62d3a27db6270cc28205
Miquéias Alves <miqueiasalves.developer@gmail.com>
2025-02-03 21:52:34 -0300

    feat: add order item repository impl

--- src/main/java/com/application/API_E_commerce/adapters/outbound/repositories/OrderItemRepositoryImplementation.java
+++ src/main/java/com/application/API_E_commerce/adapters/outbound/repositories/OrderItemRepositoryImplementation.java
@@ -1,4 +1,48 @@
 package com.application.API_E_commerce.adapters.outbound.repositories;
 
-public class OrderItemRepositoryImplementation {
+import com.application.API_E_commerce.domain.order.orderitem.OrderItem;
+import com.application.API_E_commerce.domain.order.orderitem.OrderItemRepository;
+import com.application.API_E_commerce.utils.mappers.OrderItemMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+public class OrderItemRepositoryImplementation implements OrderItemRepository {
+  private final JpaOrderItemRepository jpaOrderItemRepository;
+  private final OrderItemMapper orderItemMapper;
+
+  public OrderItemRepositoryImplementation(JpaOrderItemRepository jpaOrderItemRepository, OrderItemMapper orderItemMapper) {
+    this.jpaOrderItemRepository = jpaOrderItemRepository;
+    this.orderItemMapper = orderItemMapper;
+  }
+
+  @Override
+  public OrderItem saveOrderItem(OrderItem orderItem) {
+    return null;
+  }
+
+  @Override
+  public Optional<OrderItem> findOrderItemById(UUID orderItemId) {
+    return Optional.ofNullable(jpaOrderItemRepository.findById(orderItemId)
+            .map(orderItemMapper::toDomain)
+            .orElseThrow(() -> new IllegalArgumentException("Order item not found.")));
+  }
+
+  @Override
+  public List<OrderItem> findAllOrderItems() {
+    return List.of();
+  }
+
+  @Override
+  public void deleteOrderItemById(UUID id) {
+
+  }
+
+  @Override
+  public void deleteOrderItem(OrderItem orderItem) {
+
+  }
 }
