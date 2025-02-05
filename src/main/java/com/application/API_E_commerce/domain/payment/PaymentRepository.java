c85331f39749fe5352c196231776a707420ff7a1
Miquéias Alves <miqueiasalves.developer@gmail.com>
2025-02-05 16:25:52 -0300

    chore: add payment repository

--- src/main/java/com/application/API_E_commerce/domain/payment/PaymentRepository.java
+++ src/main/java/com/application/API_E_commerce/domain/payment/PaymentRepository.java
@@ -1,4 +1,11 @@
 package com.application.API_E_commerce.domain.payment;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface PaymentRepository {
+  Optional<Payment> findPaymentById(UUID id);
+  Optional<Payment> findPaymentByOrderId(UUID orderId);
+  Payment savePayment(Payment payment);
+  void deletePayment(UUID id);
 }
