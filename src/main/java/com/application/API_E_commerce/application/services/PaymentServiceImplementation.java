9be57df20286c1961ac5d5417a501fcd0aebf7b8
Miquéias Alves <miqueiasalves.developer@gmail.com>
2025-02-05 16:15:48 -0300

    feat: add payment service class

--- src/main/java/com/application/API_E_commerce/application/services/PaymentServiceImplementation.java
+++ src/main/java/com/application/API_E_commerce/application/services/PaymentServiceImplementation.java
@@ -1,4 +1,14 @@
 package com.application.API_E_commerce.application.services;
 
-public class PaymentServiceImplementation {
+import com.application.API_E_commerce.application.usecases.PaymentUseCases;
+import com.application.API_E_commerce.domain.payment.Payment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentServiceImplementation implements PaymentUseCases {
+  @Override
+  public Payment processPayment(Payment payment) {
+    System.out.println("Processing payment...");
+    return payment;
+  }
 }
